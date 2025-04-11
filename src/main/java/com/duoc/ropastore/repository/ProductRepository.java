72028f941599ad31dd59b614170471434f9b2804
Nicolas Silva <nic.silvaa@duocuc.cl>
2025-04-10 22:26:23 -0400

    Add methods to sort and filter products

--- src/main/java/com/duoc/ropastore/repository/ProductRepository.java
+++ src/main/java/com/duoc/ropastore/repository/ProductRepository.java
@@ -4,12 +4,14 @@ import com.duoc.ropastore.model.Product;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 public class ProductRepository {
 
-	private List<Product> products = new ArrayList<>();
+	private final List<Product> products = new ArrayList<>();
 
 	public List<Product> getProducts() {
 		return products;
@@ -17,6 +19,16 @@ public class ProductRepository {
 
 	public void addProduct(Product product) {
 		products.add(product);
+	}
+
+	public List<Product> getProductsByCategory(String category) {
+		return getProducts().stream()
+			.filter(product -> product.getCategory().equals(category)).collect(Collectors.toList());
+	}
+
+	public List<Product> sortProductsByName() {
+		return getProducts().stream()
+			.sorted(Comparator.comparing(Product::getName)).toList();
 	}
 
 	public ProductRepository(){
@@ -39,7 +51,7 @@ public class ProductRepository {
 				22.3,
 				"Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
 				"https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-				"men's clothing",
+				"clothing",
 				5
 			)
 		);
@@ -47,11 +59,11 @@ public class ProductRepository {
 		products.add(new Product(
 				3,
 				"benetton",
-				"Cotton Jacket",
+				"cotton jacket",
 				55.99,
 				"great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member.",
 				"https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
-				"men's clothing",
+				"clothing",
 				0
 			)
 		);
