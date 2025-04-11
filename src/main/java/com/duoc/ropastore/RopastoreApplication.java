0c6b3a1f9457951d6557cc3185b38adc08f7183a
Nicolas Silva <nic.silvaa@duocuc.cl>
2025-04-10 20:09:43 -0400

    First commit

---
+++ src/main/java/com/duoc/ropastore/RopastoreApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.ropastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RopastoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RopastoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duoc/ropastore/RopastoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.ropastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RopastoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
