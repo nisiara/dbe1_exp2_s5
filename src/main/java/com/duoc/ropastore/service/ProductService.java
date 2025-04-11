458da18adb2af253e216f48bcbe42960ee072b26
Nicolas Silva <nic.silvaa@duocuc.cl>
2025-04-10 22:28:50 -0400

    Create and implement methods

--- src/main/java/com/duoc/ropastore/service/ProductService.java
+++ src/main/java/com/duoc/ropastore/service/ProductService.java
@@ -1,4 +1,15 @@
 package com.duoc.ropastore.service;
 
+import com.duoc.ropastore.model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface ProductService {
+
+	List<Product> getProducts();
+	List<Product> getProductsByCategory(String category);
+	List<Product> sortProductsByName();
+
 }
--- src/main/java/com/duoc/ropastore/service/ProductServiceImplementation.java
+++ src/main/java/com/duoc/ropastore/service/ProductServiceImplementation.java
@@ -1,7 +1,31 @@
 package com.duoc.ropastore.service;
 
+import com.duoc.ropastore.model.Product;
+import com.duoc.ropastore.repository.ProductRepository;
+
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class ProductServiceImplementation {
+public class ProductServiceImplementation implements ProductService {
+
+	//Instanciamos la clase ProductService para acceder a los servicios.
+	private final ProductRepository productRepository = new ProductRepository();
+
+	@Override
+	public List<Product> getProducts(){
+		return productRepository.getProducts();
+	}
+
+	@Override
+	public List<Product> getProductsByCategory(String category){
+		return productRepository.getProductsByCategory(category);
+	}
+
+	@Override
+	public List<Product> sortProductsByName() {
+		return productRepository.sortProductsByName();
+	}
+
 }
