80cfb31ee4cd3c31693d60a69f0c114586b2bc4f
Nicolas Silva <nic.silvaa@duocuc.cl>
2025-04-10 20:43:51 -0400

    Add Product properties

--- src/main/java/com/duoc/ropastore/model/Product.java
+++ src/main/java/com/duoc/ropastore/model/Product.java
@@ -1,4 +1,20 @@
 package com.duoc.ropastore.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
+	private String id;
+	private String name;
+	private double price;
+	private String description;
+	private String imageUrl;
+	private String category;
+	private boolean hasStock;
 }
